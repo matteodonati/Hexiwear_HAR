469a1f9d32f3a88b98160f8d8de25e153e453451
MatteoDonati <matteo.donati_1@outlook.it>
2021-04-01 14:06:06 +0200

    Implementation of model.h, model.cpp and modification of the directories' structure

---
+++ application/main.cpp
@@ -0,0 +1,42 @@
+/**
+ * Libraries.
+ */
+
+/* Inclusion of mbed.h */
+#include "mbed.h"
+
+/* Inclusion of model.h */
+#include "model.h"
+
+/* Inclusion of all_ops_resolver.h. */
+#include "libraries/tensorflow/tensorflow/lite/micro/all_ops_resolver.h"
+
+/* Inclusion of micro_error_reporter.h. */
+#include "libraries/tensorflow/tensorflow/lite/micro/micro_error_reporter.h"
+
+/* Inclusion of micro_interpreter.h. */
+#include "libraries/tensorflow/tensorflow/lite/micro/micro_interpreter.h"
+
+/* Inclusion of schema/schema_generated.h. */
+#include "libraries/tensorflow/tensorflow/lite/schema/schema_generated.h"
+
+/* Inclusion of version.h. */
+#include "libraries/tensorflow/tensorflow/lite/version.h"
+
+/* Inclusion of kernel_util.h. */
+#include "libraries/tensorflow/tensorflow/lite/kernels/kernel_util.h"
+
+/* Use tflite namespace. */
+using namespace tflite;
+
+
+/**
+ * Functions definition.
+ */
+
+/* Main function. */
+int main() 
+{	
+	/* Return zero. */
+    return 0;
+}
---
+++ application/model.h
@@ -0,0 +1,9 @@
+/**
+ * Variables declaration.
+ */
+
+/* Model dimension. */
+extern const int g_model_len;
+
+/* Model. */
+extern const unsigned char g_model[];
